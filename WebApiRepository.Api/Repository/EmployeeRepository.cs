4bc2c10125c1decf694fc209e0e83f6e246c6c70
David <david_horeb@hotmail.com>
2021-10-27 06:50:17 -0300

    Parte 1 de Repository Empleados

---
+++ WebApiRepository.Api/Repository/EmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiRepository.Api.Models;
+
+namespace WebApiRepository.Api.Repository
+{
+    public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(ApplicationDBContext applicationDBContext) : base(applicationDBContext)
+        {
+
+        }
+    }
+}
---
+++ WebApiRepository.Api/Repository/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiRepository.Api.Models;
+
+namespace WebApiRepository.Api.Repository
+{
+    public interface IEmployeeRepository : IRepositoryBase<Employee>
+    {
+    }
+}
